48f0a270ad15d4594c22015670d37663c2b73d7f
levnikonenko <levnikonenko@yandex.ru>
2024-12-01 18:48:19 +0300

    - Реализовал сервис по получению метеорологических данных

--- src/main/java/weather/example/WeatherApp.java
+++ src/main/java/weather/example/WeatherApp.java
@@ -4,25 +4,73 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
-public class WeatherServlet {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WeatherApp {
     public static void main(String[] args) {
+        String apiKey = "4429618b-46fe-4286-bc5b-07e373775f74";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите долготу в градусах в формате XX.XX: ");
+        String lon = scanner.nextLine();
+        System.out.println("Введите широту в градусах в формате XX.XX: ");
+        String lat = scanner.nextLine();
+        System.out.println("Введите количество дней: ");
+        int limit = scanner.nextInt();
+        String url = String.format("https://api.weather.yandex.ru/v2/forecast?lat=%s&lon=%s&limit=%d", lon, lat, limit);
+
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.weather.yandex.ru/v2/forecast?lat=55.75&lon=37.62&limit=1"))
-                .headers("Content-Type","application/json")
-                .headers("X-Yandex-Weather-Key", "4429618b-46fe-4286-bc5b-07e373775f74")
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("X-Yandex-Weather-Key", apiKey)
                 .GET()
                 .build();
 
         try {
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Response Code: " + response.statusCode());
-            System.out.println("Response Body: " + response.body());
-            System.out.println("Length Response Body: " + response.body().length());
 
+            if (response.statusCode() == 200) {
+                String responseBody = response.body();
+                System.out.println("Данные с сервера: " + responseBody);
+
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode rootNode = objectMapper.readTree(responseBody);
+
+                JsonNode factNode = rootNode.path("fact");
+                System.out.println("Текущая температура: " + factNode.path("temp").asDouble() + "°C");
+
+                JsonNode forecasts = rootNode.path("forecasts");
+
+                if (forecasts.isArray() && !forecasts.isEmpty()) {
+                    JsonNode forecast = forecasts.get(0);
+                    JsonNode parts = forecast.path("parts");
+
+                    if (parts.isObject()) {
+                        double totalTemp = 0;
+                        int count = 0;
+
+                        for (JsonNode part : parts) {
+                            double tempAvg = part.path("temp_avg").asDouble();
+                            totalTemp += tempAvg;
+                            count++;
+                        }
+
+                        if (count > 0) {
+                            double avgTemp = totalTemp / count;
+                            System.out.printf("Средняя температура за период: %.2f°C%n", avgTemp);
+                        }
+                    }
+                }
+
+            } else {
+                System.err.println("Ошибка: Получен код ответа " + response.statusCode());
+            }
         } catch (Exception e) {
-            System.err.println("Error making HTTP request: " + e.getMessage());
+            System.err.println("Ошибка: " + e.getMessage());
         }
     }
 }
